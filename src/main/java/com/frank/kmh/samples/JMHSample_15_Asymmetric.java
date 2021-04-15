ba6fcdc56515dda7993faedf045cc04b7b6f6c4c
zhengguojun <zgj>
2021-04-15 08:36:52 +0800

    demo 13-18

---
+++ src/main/java/com/frank/kmh/samples/JMHSample_15_Asymmetric.java
@@ -0,0 +1,47 @@
+package com.frank.kmh.samples;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@State(Scope.Group)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class JMHSample_15_Asymmetric {
+
+    private AtomicInteger counter;
+
+    @Setup
+    public void up() {
+        counter = new AtomicInteger();
+    }
+
+    @Benchmark
+    @Group("g")
+    @GroupThreads(3)
+    public int inc() {
+        return counter.incrementAndGet();
+    }
+
+    @Benchmark
+    @Group("g")
+    @GroupThreads(1)
+    public int get() {
+        return counter.get();
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(JMHSample_15_Asymmetric.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+}
